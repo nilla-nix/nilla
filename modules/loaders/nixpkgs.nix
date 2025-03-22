bb10274f0298d6be7e5cd68d2768c72a967c65f4
Jake Hamilton <jake.hamilton@hey.com>
2025-03-22 06:12:30 -0700

    refactor: use shorthand and non-conflicting configuration name

--- modules/loaders/nixpkgs.nix
+++ modules/loaders/nixpkgs.nix
@@ -6,32 +6,42 @@ in
   config = {
     loaders.nixpkgs = {
       settings = {
-        type = lib.types.submodule {
-          options = {
-            system = lib.options.create {
-              description = "The system to build the package set for.";
-              type = lib.types.string;
-              default.value = "x86_64-linux";
-            };
+        type = lib.types.submodules.of {
+          shorthand = true;
+          modules = [
+            {
+              options = {
+                system = lib.options.create {
+                  description = "The system to build the package set for.";
+                  type = lib.types.string;
+                  default.value = "x86_64-linux";
+                };
 
-            overlays = lib.options.create {
-              description = "A list of overlays to apply to the package set.";
-              type = lib.types.list.of (lib.types.function lib.types.raw);
-              default.value = [ ];
-            };
+                overlays = lib.options.create {
+                  description = "A list of overlays to apply to the package set.";
+                  type = lib.types.list.of (lib.types.function lib.types.raw);
+                  default.value = [ ];
+                };
 
-            config = lib.options.create {
-              description = "Configuration to apply to the package set.";
-              type = lib.types.attrs.any;
-              default.value = { };
-            };
-          };
+                # NOTE: We can't call this `config` due to that being a reserved word in the module
+                # system. It's not pretty, but it works.
+                configuration = lib.options.create {
+                  description = "Configuration to apply to the package set.";
+                  type = lib.types.attrs.any;
+                  default.value = { };
+                };
+              };
+            }
+          ];
         };
 
         default = { };
       };
 
-      load = input: import input.src input.settings;
+      load = input: import input.src {
+        inherit (input.settings) system overlays;
+        config = input.settings.configuration;
+      };
     };
   };
 }
