e22d9f091397d27af80ce79051fd1be9452f598a
Jake Hamilton <jake.hamilton@hey.com>
2025-05-13 01:22:58 -0700

    feat: add support for replacing flake inputs (#21)

--- examples/base/nilla.nix
+++ examples/base/nilla.nix
@@ -1,4 +1,5 @@
 let
+  pins = import ../../npins;
   nilla = import ../../default.nix;
 
   result =
@@ -6,17 +7,7 @@ let
       config = {
         inputs = {
           nixpkgs = {
-            src = builtins.fetchTarball {
-              url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz";
-              sha256 = "013q5l72i020y3b7sdw1naiqwxm4h29alwlzkv4jsnb2k7qmwbdf";
-            };
-          };
-
-          plusultra = {
-            src = builtins.fetchTarball {
-              url = "https://github.com/jakehamilton/config/archive/main.tar.gz";
-              sha256 = "1z50qmmc2hnqjc53cpaijxn16dlyyx5iglvi3xwri4j8dwg98fnc";
-            };
+            src = pins.nixpkgs;
           };
         };
       };
