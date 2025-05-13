97d429d30c28687f5da505acb4407791bfd53064
Jake Hamilton <jake.hamilton@hey.com>
2025-05-13 01:32:18 -0700

    chore: bump version

--- modules/nilla/default.nix
+++ modules/nilla/default.nix
@@ -5,7 +5,7 @@
       description = "The version of Nilla used.";
       type = lib.types.string;
       writable = false;
-      default.value = "0.0.0-alpha.10";
+      default.value = "0.0.0-alpha.11";
     };
   };
 }
